bdb89089707023c9e8b879ceac5d36f496966ed4
Gudipati Vishnu Vardhan Reddy <vishnureddy228@gmail.com>
2023-08-22 21:05:52 +0530

    Create and change

---
+++ EDIT.py
@@ -0,0 +1,10 @@
+
+with open("Dataset\\r1.txt", "r") as file:
+    ref = file.read()
+    print(str(ref))
+
+
+def add(*x):
+    ref = str(ref)
+    tags = ["ENAME", "YNAME", "YWORK", "YPLACE", "YJOB",
+            "YROLE", "YJOB", "YX", "YROLE", "YFNAME"]
