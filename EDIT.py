9eaaeeaad254e81af2e12a879fce0432f87feb19
Gudipati Vishnu Vardhan Reddy <vishnureddy228@gmail.com>
2023-08-22 21:07:29 +0530

    edit modified

--- EDIT.py
+++ EDIT.py
@@ -8,3 +8,5 @@ def add(*x):
     ref = str(ref)
     tags = ["ENAME", "YNAME", "YWORK", "YPLACE", "YJOB",
             "YROLE", "YJOB", "YX", "YROLE", "YFNAME"]
+    for i in tags:
+        ref.replace(tags[i], x[i])
